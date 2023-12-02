651ffb472ceb639732ee3ba81e321ad900e30ec5
ljx <lijiaxing@mail.ustc.edu.cn>
2023-12-02 10:41:25 +0800

    创建仓库

---
+++ src/main/java/com/ljx/Main.java
@@ -0,0 +1,11 @@
+package com.ljx;
+
+/**
+ * @Author: ljx
+ * @Date: 2023/12/2 10:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
